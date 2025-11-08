a6f3c855d640c1307d08ee7d73aff35906ca47f6
Wilian Huanca <whuanca@okode.com>
2025-11-08 01:32:32 +0100

    feat(perms): Test comunicate plugin

--- ios/Sources/ResourcePlugin/Resource.swift
+++ ios/Sources/ResourcePlugin/Resource.swift
@@ -2,7 +2,7 @@ import Foundation
 
 @objc public class Resource: NSObject {
     @objc public func echo(_ value: String) -> String {
-        print(value)
+        print("FROM XCODE" + value)
         return value
     }
 }
